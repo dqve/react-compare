c6216ee9a83a7c4fc344d6d832bb6fb63ff3d7b0
dqve <ayooladavid@gmail.com>
2020-08-15 01:16:49 +0100

    init files

--- src/App/layout/AdminLayout/NavBar/NavRight/index.js
+++ src/App/layout/AdminLayout/NavBar/NavRight/index.js
@@ -6,8 +6,6 @@ import Aux from "../../../../../hoc/_Aux";
 import DEMO from "../../../../../store/constant";
 
 import Avatar1 from '../../../../../assets/images/user/avatar-1.jpg';
-import Avatar2 from '../../../../../assets/images/user/avatar-2.jpg';
-import Avatar3 from '../../../../../assets/images/user/avatar-3.jpg';
 
 class NavRight extends Component {
     state = {
@@ -32,52 +30,11 @@ class NavRight extends Component {
                                         <a href={DEMO.BLANK_LINK}>clear all</a>
                                     </div>
                                 </div>
-                                <ul className="noti-body">
-                                    <li className="n-title">
-                                        <p className="m-b-0">NEW</p>
-                                    </li>
-                                    <li className="notification">
-                                        <div className="media">
-                                            <img className="img-radius" src={Avatar1} alt="Generic placeholder"/>
-                                            <div className="media-body">
-                                                <p><strong>John Doe</strong><span className="n-time text-muted"><i
-                                                    className="icon feather icon-clock m-r-10"/>30 min</span></p>
-                                                <p>New ticket Added</p>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li className="n-title">
-                                        <p className="m-b-0">EARLIER</p>
-                                    </li>
-                                    <li className="notification">
-                                        <div className="media">
-                                            <img className="img-radius" src={Avatar2} alt="Generic placeholder"/>
-                                            <div className="media-body">
-                                                <p><strong>Joseph William</strong><span className="n-time text-muted"><i
-                                                    className="icon feather icon-clock m-r-10"/>30 min</span></p>
-                                                <p>Prchace New Theme and make payment</p>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li className="notification">
-                                        <div className="media">
-                                            <img className="img-radius" src={Avatar3} alt="Generic placeholder"/>
-                                            <div className="media-body">
-                                                <p><strong>Sara Soudein</strong><span className="n-time text-muted"><i
-                                                    className="icon feather icon-clock m-r-10"/>30 min</span></p>
-                                                <p>currently login</p>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                                <div className="noti-footer">
+                               <div className="noti-footer">
                                     <a href={DEMO.BLANK_LINK}>show all</a>
                                 </div>
                             </Dropdown.Menu>
                         </Dropdown>
-                    </li>
-                    <li className={this.props.rtlLayout ? 'm-r-15' : 'm-l-15'}>
-                        <a href={DEMO.BLANK_LINK} className="displayChatbox" onClick={() => {this.setState({listOpen: true});}}><i className="icon feather icon-mail"/></a>
                     </li>
                     <li>
                         <Dropdown alignRight={!this.props.rtlLayout} className="drp-user">
@@ -95,8 +52,6 @@ class NavRight extends Component {
                                 <ul className="pro-body">
                                     <li><a href={DEMO.BLANK_LINK} className="dropdown-item"><i className="feather icon-settings"/> Settings</a></li>
                                     <li><a href={DEMO.BLANK_LINK} className="dropdown-item"><i className="feather icon-user"/> Profile</a></li>
-                                    <li><a href={DEMO.BLANK_LINK} className="dropdown-item"><i className="feather icon-mail"/> My Messages</a></li>
-                                    <li><a href={DEMO.BLANK_LINK} className="dropdown-item"><i className="feather icon-lock"/> Lock Screen</a></li>
                                 </ul>
                             </Dropdown.Menu>
                         </Dropdown>
